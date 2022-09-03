dbef12a521b55f7c09560e0856ac322130ea2139
arfan36 <arfanur36@gmail.com>
2022-09-04 02:26:44 +0600

    complete main task

--- js/app.js
+++ js/app.js
@@ -20,7 +20,9 @@ const displayCategories = (categories) => {
 
         const div = document.createElement('div');
         div.innerHTML = `
-        <button onclick="categoryDetails(${category.category_id})" class="btn">${category.category_name}</button>
+        <div class="mb-5">
+            <button onclick="categoryDetails(${category.category_id})" class="btn">${category.category_name}</button>
+        </div>
         `;
         newsCategory.appendChild(div);
     });
@@ -48,9 +50,9 @@ const displayCategoryDetails = (sameCategories) => {
         // console.log(category._id);
         const div = document.createElement('div');
         div.innerHTML = `
-        <div onclick="itemDetails('${category._id}')" class="card mb-3" style="max-width: auto;">
+        <div onclick="itemDetails('${category._id}')" class="card mb-3" style="max-width: auto;" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-md-4 d-flex align-items-center">
                   <img src="${category.image_url}" class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
@@ -63,7 +65,7 @@ const displayCategoryDetails = (sameCategories) => {
                                     <img src="${category.author.img}" class="rounded-circle" style="width: 50px">
                                 </div>
                                 <div class="ms-2">
-                                    <p>${category.author.name}</p>
+                                    <p>${category.author.name ? category.author.name : 'no data found'}</p>
                                     <p>${category.author.published_date}</p>
                                 </div>
                             </div>
@@ -82,16 +84,60 @@ const displayCategoryDetails = (sameCategories) => {
         newsContainer.appendChild(div);
     });
 };
-categoryDetails();
+// categoryDetails();
 
 // ---------------------------------------------------- item details
-const itemDetails = async (itemId) => {
-    const url = `https://openapi.programming-hero.com/api/news/${itemId}`;
+const itemDetails = async (newsId) => {
+    // console.log(newsId);
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
     try {
         const res = await fetch(url);
-
+        const data = await res.json();
+        displayItemDetails(data.data[0]);
     }
     catch (error) {
         console.log(error);
     }
+};
+const displayItemDetails = (itemDetail) => {
+    // console.log(itemDetail);
+    const modalDetail = document.getElementById('news-details');
+    modalDetail.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `    
+
+    <div class="card mb-3" style="max-width: auto;">
+                <div class="row g-0">
+                    <div class="col-md-12">
+                        <img src="${itemDetail.image_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-12">
+                        <div class="card-body">
+                            <h5 class="card-title">${itemDetail.title}</h5>
+                            <p class="card-text">${itemDetail.details.length > 200 ? itemDetail.details.slice(0, 200) + '...' : itemDetail.details}</p>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <img src="${itemDetail.author.img}" class="rounded-circle" style="width: 50px">
+                                    </div>
+                                    <div class="ms-2">
+                                        <p>${itemDetail.author.name ? itemDetail.author.name : 'no data found'}</p>
+                                        <p>${itemDetail.author.published_date}</p>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center">
+                                    <p><i class="fa-regular fa-eye"></i></p>
+                                    <p class="ms-1">${itemDetail.total_view ? itemDetail.total_view : 'No data found'}</p>
+                                </div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
+     `;
+    modalDetail.appendChild(div);
+
 };
